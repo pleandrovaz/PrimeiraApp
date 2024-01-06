d2dca5fbfa2f6cfb727f9ba2bac9af39bdb71b9f
pleandrovaz <pleandrovaz@gmail.com>
2024-01-06 16:23:28 -0300

    Add files via upload

---
+++ Controllers/TesteEFController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiraApp.Data;
+using PrimeiraApp.Models;
+
+namespace PrimeiraApp.Controllers
+{
+    public class TesteEFController : Controller
+    {
+        public AppDbContext Db { get;set; }
+        public TesteEFController(AppDbContext db)
+        {
+            Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Ativo = true,
+                Avaliacao = 9,
+                DataNascimento = new DateTime(1973, 12, 11),
+                Email="teste@gmail.com",
+                Nome="Paulo L. V. Teixeira"
+            };
+
+
+            Db.Alunos.Add(aluno);
+            Db.SaveChanges();
+
+            return View();
+        }
+    }
+}
